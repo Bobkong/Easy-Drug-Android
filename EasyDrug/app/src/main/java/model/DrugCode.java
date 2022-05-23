2da28f0797e4fc24e9453b5420e1684536d83fed
carson <gul006@ucsd.edu>
2022-05-22 19:54:10 -0700

    DrugApi finished

---
+++ EasyDrug/app/src/main/java/model/DrugCode.java
@@ -0,0 +1,33 @@
+package model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class DrugCode implements Serializable {
+    @SerializedName("username")
+    private String userName;
+    @SerializedName("drug_upc_code")
+    private String drugUpcCode;
+
+    public DrugCode(String userName, String drugUpcCode) {
+        this.userName = userName;
+        this.drugUpcCode = drugUpcCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDrugUpcCode() {
+        return drugUpcCode;
+    }
+
+    public void setDrugUpcCode(String drugUpcCode) {
+        this.drugUpcCode = drugUpcCode;
+    }
+}
